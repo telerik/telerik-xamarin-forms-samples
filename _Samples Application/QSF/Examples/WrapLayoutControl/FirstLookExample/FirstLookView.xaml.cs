15759dd5049553ca137cdc3fe71daf849147c0da
ProgressGitHubBuildUser <Petia.Vladova@progress.com>
2018-11-09 15:27:10 +0200

    New version of Progress Telerik UI for Xamarin QSF examples.

---
+++ _Samples Application/QSF/Examples/WrapLayoutControl/FirstLookExample/FirstLookView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QSF.Examples.WrapLayoutControl.FirstLookExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstLookView : ContentView
+    {
+        public FirstLookView()
+        {
+            InitializeComponent();
+        }
+    }
+}
