71caa50af8a263e06232ef35127874c576c879f6
atanas.popatanasov <atanas.popatanasov@progress.com>
2020-02-05 14:03:45 +0200

    Added TODO comments for the constants that are needed for the ERP app to be started as expected. Throw an exception if values are not provided.

--- ERP/app/ErpApp/ErpApp/Constants.cs
+++ ERP/app/ErpApp/ErpApp/Constants.cs
@@ -4,9 +4,13 @@ namespace ErpApp
 {
     public static class Constants
     {
-        public static string ApplicationURL = "ENTER_YOUR_APP_URL";
+        // TODO: Replace with your Azure Mobile App endpoint
+        public static string ApplicationURL = "";
+        // TODO: Provide image URL for the avatar of the customer
         public static readonly Uri EmptyCustomerImage = new Uri("ENTER_YOUR_CUSTOMER_AVATAR_URL");
+        // TODO: Provide image URL for the avatar of the product
         public static readonly Uri EmptyProductImage = new Uri("ENTER_YOUR_PRODUCT_AVATAR_URL");
+        // TODO: Provide image URL for the avatar of the vendor
         public static readonly Uri EmptyVendorImage = new Uri("ENTER_YOUR_VENDOR_AVATAR_URL");
     }
 }
