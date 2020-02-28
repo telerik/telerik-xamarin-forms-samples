daa9afde96f880162a46d9ef8678554d4c26cb85
atanas.popatanasov <atanas.popatanasov@progress.com>
2020-02-28 17:29:32 +0200

    Set the constant URIs to null in order to prevent an exception.

--- ERP/app/ErpApp/ErpApp/Constants.cs
+++ ERP/app/ErpApp/ErpApp/Constants.cs
@@ -7,10 +7,10 @@ namespace ErpApp
         // TODO: Replace with your Azure Mobile App endpoint
         public static string ApplicationURL = "";
         // TODO: Provide image URL for the avatar of the customer
-        public static readonly Uri EmptyCustomerImage = new Uri("ENTER_YOUR_CUSTOMER_AVATAR_URL");
+        public static readonly Uri EmptyCustomerImage = null;
         // TODO: Provide image URL for the avatar of the product
-        public static readonly Uri EmptyProductImage = new Uri("ENTER_YOUR_PRODUCT_AVATAR_URL");
+        public static readonly Uri EmptyProductImage = null;
         // TODO: Provide image URL for the avatar of the vendor
-        public static readonly Uri EmptyVendorImage = new Uri("ENTER_YOUR_VENDOR_AVATAR_URL");
+        public static readonly Uri EmptyVendorImage = null;
     }
 }
